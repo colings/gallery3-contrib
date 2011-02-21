423093c0eb3a813166a02ba7cf82c9d87a87666c
colings <colings@example.com>
2011-02-21 12:00:32 -0600

    Updated to latest gallery name

--- 3.0/modules/embed_videos/helpers/embed_videos_theme.php
+++ 3.0/modules/embed_videos/helpers/embed_videos_theme.php
@@ -18,7 +18,7 @@
  * Foundation, Inc., 51 Franklin Street - Fifth Floor, Boston, MA  02110-1301, USA.
  */
 class embed_videos_theme_Core {
-  static function head($theme) {
+  static function footer($theme) {
     $item = $theme->item();
     if ($item && $item->is_photo()) {
       $embedded_video = ORM::factory("embedded_video")
